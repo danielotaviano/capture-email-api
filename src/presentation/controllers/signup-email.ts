b224d06dce454d3799f70b37edf83c70d0007d63
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-11 19:57:15 -0300

    feat: ensure SignUpEmailController return an error if no name is provided

--- src/presentation/controllers/signup-email.ts
+++ src/presentation/controllers/signup-email.ts
@@ -2,7 +2,8 @@
 export class SignUpEmailController {
   handle(httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: email')
     }
   }
 }
