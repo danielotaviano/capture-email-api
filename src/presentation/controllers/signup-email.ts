051b4d3d9b1937a5198014c535672dfbc3791f72
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-11 20:19:56 -0300

    feat: ensure SignUpEmailController calls EmailValidator with correct email

--- src/presentation/controllers/signup-email.ts
+++ src/presentation/controllers/signup-email.ts
@@ -1,9 +1,16 @@
 import { MissingParamError } from '../err/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpEmailController {
+  private readonly emailValidator: EmailValidator
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
+    this.emailValidator.isValid(httpRequest.body.email)
     return badRequest(new MissingParamError('email'))
   }
 }
