9a70bfc99d8659a736c8cb912edf802fdeccc0ad
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-12 18:29:18 -0300

    test: ensure MongodbHelper reconnect if mongodb is down

---
+++ src/infra/db/mongoose/helpers/mongodb-helper.spec.ts
@@ -0,0 +1,26 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { MongooseHelper as sut } from './mongodb-helper'
+
+describe('MongodbHelper', () => {
+  const mongod = new MongoMemoryServer()
+  beforeAll(async () => {
+    const uri = await mongod.getUri()
+    await sut.connect(uri)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+    await mongod.stop()
+  })
+  test('should reconnect if mongodb is down', async () => {
+    let emailsCollection = await sut.getCollection('emails')
+
+    expect(emailsCollection).toBeTruthy()
+
+    await sut.disconnect()
+
+    emailsCollection = await sut.getCollection('emails')
+
+    expect(emailsCollection).toBeTruthy()
+  })
+})
